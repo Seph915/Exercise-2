29802028a343c9d5bf4f799ac81f12e4606c1f0b
fabio <fabiocostanza91@gmail.com>
2023-07-14 13:31:04 +0200

    main

---
+++ src/co/develehope/introduction/Main.java
@@ -0,0 +1,7 @@
+package co.develehope.introduction;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello develhope java student" + "20 maggio 1991");
+    }
+}
---
+++ src/co/develehope/introduction/Start.java
@@ -0,0 +1,4 @@
+package co.develehope.introduction;
+
+public class Start {
+}
